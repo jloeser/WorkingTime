e9674683581677258513872280961c5190c565e9
aneisner <antonio.eisner@suse.com>
2024-02-27 10:14:36 +0100

    Working on methods in day-managment.py to handel workingdays an git handling

--- src/day-managment.py
+++ src/day-managment.py
@@ -1,5 +1,25 @@
+class GitHandling:
+
+    def workdays_pull(self):
+        pass
+
+    def workdays_push(self):
+        pass
+
+
 class ManagementWorkingDays:
     
+    def workingdays_list(self):
+        pass
+
+    def workingday_add(self):
+        pass
+
+    def workingday_edit(self):
+        pass
+
+    def workingdays_del(self):
+        pass
 
 
 class WorkHours:
